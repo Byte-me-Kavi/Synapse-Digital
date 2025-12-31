3675bc15f8134f72354f19da7540745aaaa4737e
Kavisha Trishan <kaveeshatmdss@gmail.com>
2025-12-31 18:41:27 +0530

    removed traacker

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,13 +110,13 @@ export default function RootLayout({
         <JsonLd />
         <div className="relative z-10 w-full">{children}</div>
 
-        <script 
+        {/* <script 
           async 
           src="https://navlens-rho.vercel.app/tracker.js" 
           data-site-id="a2ec32a6-21bb-4bdc-b89e-c09145df7fac"
           data-api-key="89c0fb13-a17e-4b96-8d59-8fba10f51fed"
           data-api-host="https://navlens-rho.vercel.app"
-        ></script>
+        ></script> */}
       </body>
     </html>
   );
